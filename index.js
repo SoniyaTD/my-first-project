02543140f242a30466123f04a10dd1ce27d1f273
SoniyaTD <soniyarj9416@gmail.com>
2025-11-17 13:34:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const VERIFY_TOKEN = "twodimension";
+
+app.get("/webhook", (req, res) => {
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+  if (token === VERIFY_TOKEN) return res.send(challenge);
+  res.sendStatus(403);
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("Incoming:", JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
