1ea2afd8cbb427ca154c49eda31de6d775034412
SoniyaTD <soniyarj9416@gmail.com>
2025-11-17 14:44:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,4 +16,6 @@ app.post("/webhook", (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Server running"));
+
+
